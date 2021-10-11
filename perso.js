592c839bb761915dac5a7d49a48bfc14790c76eb
Evann Nalewajek <evann.nalewajek@gmail.com>
2021-10-11 16:38:11 +0200

    commitperso10

--- perso.js
+++ perso.js
@@ -1,21 +1,32 @@
 class Produit {
-    constructor(name, id) {
+    constructor(name, id, img, price) {
         this.name = name;
         this.id = id;
+        this.img = img;
+        this.price = price;
     }
 }
 
 var board = [
-    new Produit("Skateboard", sk),
-    new Produit("Longboard Carving", lg1),
-    new Produit("Longboard", lg2),
-    new Produit("Mini-Cruiser", mi)
+    new Produit("Skateboard", "sk", "skateboard-removebg-preview.png", 69.99),
+    new Produit("Longboard Carving", "lg1", "longboard1-removebg-preview.png", 99.99),
+    new Produit("Longboard", "lg2", "longboard2-removebg-preview.png", 119.99),
+    new Produit("Mini-Cruiser", "mi", "mini-cruiser-removebg-preview.png", 44.99)
 ]
 
-
-for (let x in board) {
-}
-
 function recupid() {
-    let produit_id = new URLSearchParams(window.location.search).get("id")
+    let produit_id = new URLSearchParams(window.location.search).get("id");
+    console.log(produit_id);
+    var nom;
+    var image;
+    var prix;
+    for (let x in board) {
+        if (produit_id==board[x][1]) {
+            nom = board[x][0];
+            image = board[x][2];
+            prix = board[x][3];
+        }
+    }
+    console.log(nom, image, prix);
 }
+
