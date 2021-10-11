b95c2817ba513bcdf115209f5e8f658131a6b5a7
Evann Nalewajek <evann.nalewajek@gmail.com>
2021-10-11 15:03:23 +0200

    CommitJS

--- perso.js
+++ perso.js
@@ -1,1 +1,16 @@
-let produit_id = new URLSearchParams(window.location.search).get("id")
+class Produit {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+    }
+}
+
+let skate = new Produit("Skateboard", sk)
+let long1 = new Produit("Longboard Carving", lg1)
+let long2 = new Produit("Longboard", lg2)
+let mini = new Produit("Mini-Cruiser", mi)
+
+let sk = new URLSearchParams(window.location.search).get("sk")
+let lg1 = new URLSearchParams(window.location.search).get("lg1")
+let lg2 = new URLSearchParams(window.location.search).get("lg2")
+let mi = new URLSearchParams(window.location.search).get("mi")
