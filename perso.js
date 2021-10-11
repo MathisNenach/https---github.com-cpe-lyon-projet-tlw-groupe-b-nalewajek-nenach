58cb225a30d85ab548df1810e33457603bfef17e
Evann Nalewajek <evann.nalewajek@gmail.com>
2021-10-11 15:56:19 +0200

    CommitPrincipal

--- perso.js
+++ perso.js
@@ -5,12 +5,17 @@ class Produit {
     }
 }
 
-let skate = new Produit("Skateboard", sk)
-let long1 = new Produit("Longboard Carving", lg1)
-let long2 = new Produit("Longboard", lg2)
-let mini = new Produit("Mini-Cruiser", mi)
+var board = [
+    new Produit("Skateboard", sk),
+    new Produit("Longboard Carving", lg1),
+    new Produit("Longboard", lg2),
+    new Produit("Mini-Cruiser", mi)
+]
 
-let sk = new URLSearchParams(window.location.search).get("sk")
-let lg1 = new URLSearchParams(window.location.search).get("lg1")
-let lg2 = new URLSearchParams(window.location.search).get("lg2")
-let mi = new URLSearchParams(window.location.search).get("mi")
+
+for (let x in board) {
+}
+
+function recupid() {
+    let produit_id = new URLSearchParams(window.location.search).get("id")
+}
